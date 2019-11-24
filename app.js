8b00f766503b0a2780073115325c1b39b33b9a2f
kemotj <toomasz.jarosz@gmail.com>
2019-11-24 02:22:36 +0100

    form-input citi

--- app.js
+++ app.js
@@ -1,3 +1,85 @@
+const displayExactPlaceWeather = (city) => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let iconPlace = document.querySelector('.icon');
+    // let citi = document.getElementById
+    console.log(city);
+
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(position => {
+    //         console.log(position);
+            // long = position.coords.longitude;
+            // lat = position.coords.latitude;
+            const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pl&APPID=342e58ac10157a7f0274028550e55fc5`
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    let {
+                        temp
+                    } = data.main;
+                    let {
+                        description
+                    } = data.weather[0];
+                    let {
+                        country
+                    } = data.sys;
+                    let {
+                        icon
+                    } = data.weather[0]
+                    temperatureDegree.textContent = temp;
+                    temperatureDescription.textContent = description;
+                    locationTimezone.textContent = `${data.name} ${country} `;
+
+                    function addIcon() {
+                        const x = document.createElement("iconImg");
+                        x.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+                        x.setAttribute("width", "120");
+                        x.setAttribute("height", "120");
+                        x.setAttribute("alt", "Weather Icon");
+                        let oldIcon = iconPlace.firstChild;
+                        console.log(oldIcon);
+                        if (oldIcon==undefined)
+                            iconPlace.appendChild(x);
+                        else
+                            iconPlace.replaceChild(x, oldIcon)
+                    }
+                    addIcon();
+                });
+
+            function initMap() {
+                long = position.coords.longitude;
+                lat = position.coords.latitude;
+                // The location of Uluru
+                let myPosition = {
+                    lat: lat,
+                    lng: long
+                };
+                // The map, centered at Uluru
+                let map = new google.maps.Map(
+                    document.getElementById('map'), {
+                        zoom: 4,
+                        center: myPosition
+                    });
+                // The marker, positioned at Uluru
+                let marker = new google.maps.Marker({
+                    position: myPosition,
+                    map: map
+                });
+            }
+            // initMap();
+        };
+
+//     }
+
+
+// }
+
 const displayLocalWeather = () => {
     let long;
     let lat;
@@ -40,7 +122,12 @@ const displayLocalWeather = () => {
                         x.setAttribute("width", "120");
                         x.setAttribute("height", "120");
                         x.setAttribute("alt", "Weather Icon");
-                        iconPlace.appendChild(x);
+                        let oldIcon = iconPlace.firstChild;
+                        console.log(oldIcon);
+                        if (oldIcon==undefined)
+                            iconPlace.appendChild(x);
+                        else
+                            iconPlace.replaceChild(x, oldIcon)
                     }
                     addIcon();
                 });
